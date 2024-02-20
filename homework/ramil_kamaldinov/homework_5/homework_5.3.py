d7b7f1e19e24c1fffb973289ba8c69b0609d4c1f
R23 <kamaldinov.ramil@gmail.com>
2024-02-20 13:21:21 +0700

    homework_5 is done

---
+++ homework/ramil_kamaldinov/homework_5/homework_5.3.py
@@ -0,0 +1,6 @@
+students = ['Ivanov', 'Petrov', 'Sidorov']
+subjects = ['math', 'biology', 'geography']
+students = ', '.join(students)
+subjects = ', '.join(subjects)
+text = f'Students {students} study these subjects: {subjects}'
+print(text)
