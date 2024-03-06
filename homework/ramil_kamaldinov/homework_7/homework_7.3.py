6e5db9df9d377f5cf99973f3499e70ef4f8817da
R23 <kamaldinov.ramil@gmail.com>
2024-03-06 13:19:31 +0700

    Changed (sum to numb) and added space after (+)

--- homework/ramil_kamaldinov/homework_7/homework_7.3.py
+++ homework/ramil_kamaldinov/homework_7/homework_7.3.py
@@ -3,13 +3,13 @@ r2 = 'результат операции: 54'
 r3 = 'результат работы программы: 209'
 r4 = 'результат: 2'
 
-r11 = int(r1[r1.index(':') +2:])
-r22 = int(r2[r2.index(':') +2:])
-r33 = int(r3[r3.index(':') +2:])
-r44 = int(r4[r4.index(':') +2:])
+r11 = int(r1[r1.index(':') + 2:])
+r22 = int(r2[r2.index(':') + 2:])
+r33 = int(r3[r3.index(':') + 2:])
+r44 = int(r4[r4.index(':') + 2:])
 
-def summ(sum):
-    print(sum + 10)
+def summ(numb):
+    print(numb + 10)
 
 summ(r11)
 summ(r22)
