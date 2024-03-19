b884e6f9349ab55951151f1a8e6941ce105e3195
R23 <kamaldinov.ramil@gmail.com>
2024-03-19 13:25:34 +0700

    homework_8 is done

---
+++ homework/ramil_kamaldinov/homework_8/homework_8.2.py
@@ -0,0 +1,18 @@
+def fibonacci_generator():
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
+count = 0
+
+for x in fibonacci_generator():
+    count += 1
+    if count == 5:
+        print("Пятое число Фибоначчи:", x)
+    elif count == 200:
+        print("Двухсотое число Фибоначчи:", x)
+    elif count == 1000:
+        print("Тысячное число Фибоначчи:", x)
+    elif count == 100000:
+        print("Стотысячное число Фибоначчи:", x)
+        break
